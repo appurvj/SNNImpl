c65ec3f42b03139e2d96255c7e183066d487ab39
appurvj <appurvj@gmail.com>
2013-07-25 01:16:37 +0530

    Data class checked and working. Nedd to finish Diff Ev class method bodies
    Some debugging print statements present in code. Need to be removed in
    future

--- src/Data.java
+++ src/Data.java
@@ -1,21 +1,123 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 /**
  *
- * @author Appurv_Air
+ * @author Appurv Jain and Amrith Akula
  */
 public class Data {
-    public final double[][] trainingInput;
-    public final int[] trainingClass;
-    public final double[][] testInput;
-    public final int[] testClass;
-    public Data(double[][] train, int[] trainClass,  double[][] test, int[] testClass){
-        this.trainingInput = train;
-        this.testInput = test;
-        this.trainingClass = trainClass;
-        this.testClass = testClass;
+    public static final String SPLIT_REGEX = "[\t ,]+";
+    
+    public  double[][] trainingInput;
+    public  int[] trainingClass;
+    public  double[][] testInput;
+    public  int[] testClass;
+    public int totalInputCols;
+    public  double TRAINING_SPLIT = 0.7;
+    
+    public Data(String dataPath, int classifierCol) 
+            throws FileNotFoundException, IOException{
+        BufferedReader file = new BufferedReader(new FileReader(dataPath));
+
+        ArrayList<String> dataList = new ArrayList<String>();
+        String row = file.readLine();
+        int totalCols  = this.getNumElements(row);
+        while(row != null){
+            dataList.add(row);
+            row = file.readLine();
+        }
+        shuffle(dataList);
+       
+        int lastTrainRowIndex = (int)(dataList.size()*this.TRAINING_SPLIT);
+System.out.println("lastRowIndex Calculated " + lastTrainRowIndex);
+
+        
+System.out.println("total cols obtained : " + totalCols);
+        this.totalInputCols = totalCols - 1; 
+        
+        this.trainingInput = new double[lastTrainRowIndex + 1][this.totalInputCols];
+        this.trainingClass = new int[lastTrainRowIndex + 1];
+        
+        this.testInput = new double[dataList.size() - lastTrainRowIndex][this.totalInputCols];
+        this.testClass = new int[dataList.size() - lastTrainRowIndex];
+        
+
+        String[] indivCols;
+        for(int i = 0; i <= lastTrainRowIndex; i++){
+            indivCols = dataList.get(i).split(Data.SPLIT_REGEX);
+            int colIndex = 0;
+            for(int j = 0; j < indivCols.length; j++){
+                if(j == classifierCol)
+                    this.trainingClass[i] = Integer.parseInt(indivCols[j]);
+                else
+                    this.trainingInput[i][colIndex++] = Double.parseDouble(indivCols[j]);
+            }
+            
+        }
+        
+        int testRowIndex = lastTrainRowIndex + 1;
+        for(int i = testRowIndex; i < dataList.size(); i++){
+            indivCols = dataList.get(i).split(Data.SPLIT_REGEX);
+            int colIndex = 0;
+            for(int j = 0; j < indivCols.length; j++){
+                if(j == classifierCol)
+                    this.testClass[i - testRowIndex] = Integer.parseInt(indivCols[j]);
+                else
+                    this.testInput[i - testRowIndex][colIndex++] = Double.parseDouble(indivCols[j]);                
+            }
+        }
+        
+        this.normalize();
+    }
+    
+    private void shuffle(ArrayList<String> list){
+        Random random = new Random();
+        int index ;
+        for(int i = 0; i < list.size(); i++){
+            index = random.nextInt(list.size());
+            String temp = list.get(index);
+            list.set(index, list.get(i));
+            list.set(i, temp);
+        }
+    }
+    
+    private int getNumElements(String row){
+        return (row.split(SPLIT_REGEX)).length;       
+    }
+    
+    private void normalize(){
+        double[] maxVals = new double[this.trainingInput[0].length];
+        for(int i = 0; i < this.trainingInput.length; i++ ){
+            for(int j = 0; j < maxVals.length; j++){
+                if(maxVals[j] < this.trainingInput[i][j])
+                    maxVals[j] = this.trainingInput[i][j];
+            }
+        }
+        
+        for(int i = 0; i < this.testInput.length; i++){
+            for(int j = 0; j < maxVals.length; j++){
+                if(maxVals[j] < this.testInput[i][j])
+                    maxVals[j] = this.testInput[i][j];
+            }
+        }
+         
+        for(int i = 0; i < this.trainingInput.length; i++ ){
+            for(int j = 0; j < maxVals.length; j++){
+                this.trainingInput[i][j] /= maxVals[j];
+            }
+        }
+         
+        for(int i = 0; i < this.testInput.length; i++){
+            for(int j = 0; j < maxVals.length; j++){
+                this.testInput[i][j] /= maxVals[j];
+            }
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,9 +14,37 @@
  * @author Appurv_Air
  */
 public class Main {
-    public static void main(String[] args){
-        int[][] test = new int[5][10];
-        System.out.println("test.length = " + test.length);
-        System.out.println("test[0].length = " + test[0].length);
+    
+    
+    static int[] layerSize = {3,8,8,6};
+    static String dataLocn = "/Users/Appurv_Air/Dropbox/Coursework/CurrentCourses/AI/SNNCode/SNNImpl/src/TelaguVowel.txt";
+    public static void main(String[] args){ 
+        try{
+System.out.println("Reading File");
+            Data data = new Data(Main.dataLocn , 0);
+System.out.println("Data Ready");            
+
+            SNNController classifier = new SNNController(Main.layerSize, data);
+System.out.println("SNN controller object created ");
+        }catch(ListLengthsDifferentException e){
+            System.out.println(e.getMessage());
+        }catch(NumberFormatException n){
+            System.out.println("Number Formet excep caught!\n" + n.getMessage());
+            
+        }catch(Exception g){
+System.out.println("Exception Called in main...");        
+            System.out.println(g.getStackTrace());
+        }
+   }
+   
+    /*
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        String dataLocn = "/Users/Appurv_Air/Dropbox/Coursework/CurrentCourses/AI/SNNCode/SNNImpl/src/TelaguVowel.txt";
+        BufferedReader file;
+        file = new BufferedReader(new FileReader(dataLocn));
+        for(int i = 0; i < 10; i++)
+            System.out.println(file.readLine());
     }
+    * 
+    * */
 }
